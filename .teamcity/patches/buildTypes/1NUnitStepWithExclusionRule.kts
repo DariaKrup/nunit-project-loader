5ae043e0215f3037ca3520ce6826cc8118d59573
dkrupkina <dkrupkina@TeamCity>
2024-06-26 17:28:12 +0000

    TeamCity change in 'NUnit Runner Project / [New NUnit Console Runner] NUnit Project Loader' project: runners of '✔️ 1 NUnit step with exclusion rule' build configuration were updated

--- .teamcity/patches/buildTypes/1NUnitStepWithExclusionRule.kts
+++ .teamcity/patches/buildTypes/1NUnitStepWithExclusionRule.kts
@@ -42,6 +42,7 @@ create(DslContext.projectId, BuildType({
             id = "NUnit_1st_dll"
             nunitPath = "%teamcity.tool.NUnit.Console.3.17.0%"
             includeTests = """bin\Debug\net20\*.dll"""
+            excludeTests = """bin\Debug\net20\*-2.dll"""
             useProjectFile = true
             coverage = dotcover {
                 toolPath = "%teamcity.tool.JetBrains.dotCover.CommandLineTools.bundled%"
